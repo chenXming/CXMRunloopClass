54518ae8e636683d319cf28d6565482df1ffdd84
chenxiaoming <13716873821@163.com>
2019-05-09 15:57:44 +0800

    初始化项目，提交文件

---
+++ CXMResidentThread.h
@@ -0,0 +1,35 @@
+//
+//  CXMResidentThread.h
+//  Adas
+//
+//  Created by 陈小明 on 2019/5/8.
+//  Copyright © 2019 bitauto. All rights reserved.
+// 自封装的常驻线程
+
+#import <Foundation/Foundation.h>
+
+typedef void(^ResidentTask)(void);
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CXMResidentThread : NSObject
+
+
+/*
+ * 运行常驻线程
+ */
+- (void)run;
+
+/*
+ * 停止线程
+ */
+- (void)stop;
+
+/*
+ * 添加任务 进入线程 串行执行
+ */
+- (void)addExecuteTask:(ResidentTask)task;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CXMResidentThread.m
@@ -0,0 +1,159 @@
+//
+//  CXMResidentThread.m
+//  Adas
+//
+//  Created by 陈小明 on 2019/5/8.
+//  Copyright © 2019 bitauto. All rights reserved.
+//
+
+#import "CXMResidentThread.h"
+// 此次线程主要是为了看其是否被释放
+@interface CXMThread : NSThread
+
+@end
+
+@implementation CXMThread
+
+- (void)dealloc{
+    
+    NSLog(@"%s",__func__);
+}
+
+@end
+
+
+@interface CXMResidentThread()
+
+@property(nonatomic,strong)CXMThread *myThread;
+
+@property (assign, nonatomic, getter=isStopped) BOOL stopped;
+
+
+
+@end
+
+@implementation CXMResidentThread
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+       
+        self.stopped = NO;
+        __weak typeof(self) weakSelf = self;//防止循环引用
+        
+      //  if (@available(iOS 10.0, *)) {//iOS 10 以后
+            
+            self.myThread = [[CXMThread alloc] initWithBlock:^{
+                
+                NSLog(@"Thread  begin------%@",[NSThread currentThread]);
+                
+                //为runloop 添加source、或者 timer、或者 observer，否则runloop 回自己退出
+                [[NSRunLoop currentRunLoop] addPort:[[NSPort alloc] init] forMode:NSDefaultRunLoopMode];
+                
+                while (weakSelf && !weakSelf.isStopped) {
+                    //启动 runloop
+                    [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+
+                }
+                
+                NSLog(@"Thread end------");
+            }];
+            
+//        } else {// ios 10 以前的 执行方法
+//            // C语言方式创建
+//            self.myThread = [[CXMThread alloc] initWithBlock:^{
+//
+//                CFRunLoopSourceContext context = {0};//初始化 0
+//
+//                CFRunLoopSourceRef runloopSourceRef = CFRunLoopSourceCreate(CFAllocatorGetDefault(), 0, &context);
+//
+//                CFRunLoopAddSource(CFRunLoopGetCurrent(),runloopSourceRef, kCFRunLoopDefaultMode);
+//
+//                //启动runloop,第三个参数设置为 NO，则不需要 while 循环,第二个参数过期时间 写一个超大数。
+//                CFRunLoopRunInMode(kCFRunLoopDefaultMode, 1000000000, NO);
+//
+//            }];
+//
+//        }
+        
+    }
+    return self;
+}
+
+#pragma mark - 公共方法
+
+- (void)run{
+    
+    if(self.myThread == nil) return;
+    
+    [self.myThread start];
+    
+}
+//在 新线程执行任务
+-(void)addExecuteTask:(ResidentTask)task{
+    
+    if(self.myThread == nil || !task) return;
+    
+    //object __addExecuteTask方法要 接受传递的参数
+    [self performSelector:@selector(__addExecuteTask:) onThread:self.myThread withObject:task waitUntilDone:NO];
+    
+}
+
+- (void)stop{
+    
+    if(!self.myThread) return;
+    //waitUntilDone:YES YES：是否等 @selector 方法执行完成后继续下面的方法，NO：不需要等执行完成
+    [self performSelector:@selector(__stop) onThread:self.myThread withObject:nil waitUntilDone:YES];
+    
+}
+
+- (void)removeAllTask{
+    
+  //  [self.taskArray removeAllObjects];
+    
+}
+#pragma mark - 私有方法
+
+- (void)__stop{
+    
+    self.stopped = YES; //停止 runloop
+    // 停止当前线程，如果是在主线程调用 就会停止主线程
+    CFRunLoopStop(CFRunLoopGetCurrent());
+    self.myThread = nil;
+    
+}
+
+- (void)__addExecuteTask:(ResidentTask)task{
+    
+    task();
+    
+}
+
+- (void)dealloc{
+    
+    NSLog(@"%s",__func__);
+    
+    [self stop];
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
